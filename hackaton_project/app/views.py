90675f06837368231ef7647c584b976bcfa7188c
Alejandro-Pozos <a01656017@tec.mx>
2023-10-27 18:44:46 -0600

    db and progress in gpt3 search

--- hackaton_project/app/views.py
+++ hackaton_project/app/views.py
@@ -3,7 +3,7 @@ from app.models import *
 import openai
 from django.http import JsonResponse
 import requests
-
+from django.db.models import Q
 
 def search_crimes(request):
     query = request.GET.get('query', '') 
@@ -18,7 +18,7 @@ def search_crimes(request):
     "messages": [
         {
             "role": "system",
-            "content": f"En django tengo un modelo 'Crimes', usando esto: Crimes.objects.filter(), dame la función con los parentesis rellenados en base a esta búsqueda: {query}, la tabla tiene los campos: id, Año, Clave_Ent, Entidad, Bien_juridico_afectado, Tipo_de_delito, Subtipo_de_delito, Modalidad, Enero, Febrero, Marzo, Abril, Mayo, Junio, Agosto, Septiembre, Octubre, Noviembre, Diciembre. Los campos de 'Enero' a 'Diciembre' indican el numero de delitos ocurridos en ese mes. Mandame unicamente lo que iria en el parentesis de la funcion, no mandes ninguna explicacion, ya que la respuesta que mandes sera usada en código. Además, cualquier string debe iniciar con mayúscula."
+            "content": f"En django tengo un modelo 'Crimes', usando esto: Crimes.objects.filter(), dame la función con los parentesis rellenados en base a esta búsqueda: {query}, la tabla tiene los campos: id, Año, Clave_Ent, Entidad, Bien_juridico_afectado, Tipo_de_delito, Subtipo_de_delito, Modalidad, Enero, Febrero, Marzo, Abril, Mayo, Junio, Agosto, Septiembre, Octubre, Noviembre, Diciembre. Los campos de 'Enero' a 'Diciembre' indican el numero de delitos ocurridos en ese mes. Siempre toma el primer delito como Tipo_de_delito, si se mandan dos, el segundo como Subtipo_de_delito. Mandame unicamente lo que iria en el parentesis de la funcion, no mandes ninguna explicacion, ya que la respuesta que mandes sera usada en código. Además, cualquier string debe iniciar con mayúscula. Dado que es posible que no se encuentre un string exacto en los Tipo_de_delitos y Subtipo_de_delitos, usa icontains para solucionar esto; solo en esos casos, usa un filter predeterminado de Django para las Entidades. Usa SOLAMENTE la sintaxis predeterminada de Django."
         },
         {
             "role": "user",
